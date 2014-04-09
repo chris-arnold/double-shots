d147a95624d83b3e8e6498ea44b68e6c6d5e20a9
chris-arnold <cwa0812@gmail.com>
2014-04-08 22:14:15 -0600

    Javascript Updated

--- js/onevent_functions.js
+++ js/onevent_functions.js
@@ -155,8 +155,139 @@ $(function () {
 $("#twovtwo").click(function () {
 
 	document.getElementById("statstable").innerHTML="Nothing Here Yet.";
+
+		$.ajax({
+		type: "POST",
+		url: "stats_functions.php",
+		data: { stats: "2v2" }
+		}).done(function( msg ) {
+			$("#statstable").empty();
+		 		document.getElementById("statstable").innerHTML=msg;
+		 		sorttable.makeSortable(document.getElementById('statstable'));
+				sorttable.innerSortFunction.apply(document.getElementById("tot1v1win"), []);
+				sorttable.innerSortFunction.apply(document.getElementById("tot1v1win"), []);
+    		});
+
 	});
 });
 
+$(function () {
+$("#onevone_archive").click(function () {
 
+	document.getElementById("statstable").innerHTML="Nothing Here Yet.";
+
+		$.ajax({
+		type: "POST",
+		url: "stats_functions.php",
+		data: { archive: "1v1" }
+		}).done(function( msg ) {
+		 		document.getElementById("archive_table").innerHTML=msg;
+    		});
+
+	});
+});
+
+$(function () {
+$("#clear_archive").click(function () {
+	document.getElementById("archive_table").innerHTML="";
+	});
+});
+
+$(function () {
+$("#twovone_archive").click(function () {
+
+	document.getElementById("statstable").innerHTML="Nothing Here Yet.";
+
+		$.ajax({
+		type: "POST",
+		url: "stats_functions.php",
+		data: { archive: "2v1" }
+		}).done(function( msg ) {
+		 		document.getElementById("archive_table").innerHTML=msg;
+    		});
+
+	});
+});
+
+$(function () {
+$("#twovtwo_archive").click(function () {
+
+	document.getElementById("statstable").innerHTML="Nothing Here Yet.";
+
+		$.ajax({
+		type: "POST",
+		url: "stats_functions.php",
+		data: { archive: "2v2" }
+		}).done(function( msg ) {
+		 		document.getElementById("archive_table").innerHTML=msg;
+    		});
+
+	});
+});
+
+function archive_next_1v1(page){
+	$.ajax({
+		type: "POST",
+		url: "stats_functions.php",
+		data: { archive: "next_1v1", page: page }
+	}).done(function( msg ) {
+	//alert(msg);
+ 		document.getElementById("archive_table").innerHTML=msg;
+	});
+};
+
+function archive_previous_1v1(page){
+	$.ajax({
+		type: "POST",
+		url: "stats_functions.php",
+		data: { archive: "previous_1v1", page: page }
+	}).done(function( msg ) {
+	//alert(msg);
+ 		document.getElementById("archive_table").innerHTML=msg;
+	});
+};
+
+function archive_next_2v1(page){
+	$.ajax({
+		type: "POST",
+		url: "stats_functions.php",
+		data: { archive: "next_2v1", page: page }
+	}).done(function( msg ) {
+	//alert(msg);
+ 		document.getElementById("archive_table").innerHTML=msg;
+	});
+};
+
+function archive_previous_2v1(page){
+	$.ajax({
+		type: "POST",
+		url: "stats_functions.php",
+		data: { archive: "previous_2v1", page: page }
+	}).done(function( msg ) {
+	//alert(msg);
+ 		document.getElementById("archive_table").innerHTML=msg;
+	});
+};
+
+function archive_next_2v2(page){
+	$.ajax({
+		type: "POST",
+		url: "stats_functions.php",
+		data: { archive: "next_2v2", page: page }
+	}).done(function( msg ) {
+	//alert(msg);
+ 		document.getElementById("archive_table").innerHTML=msg;
+	});
+};
+
+function archive_previous_2v2(page){
+	$.ajax({
+		type: "POST",
+		url: "stats_functions.php",
+		data: { archive: "previous_2v2", page: page }
+	}).done(function( msg ) {
+	//alert(msg);
+ 		document.getElementById("archive_table").innerHTML=msg;
+	});
+};
 
